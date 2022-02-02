1d4cd2cea7fc2e481ed2ad211307808e9ef416ca
divyar <divyar@NICDM205.nousinfo.com>
2022-02-02 12:10:26 +0530

    HybridCucumber first commit

---
+++ HybridCucumber/src/main/java/Baseclass/Libraryclass.java
@@ -0,0 +1,10 @@
+package Baseclass;
+
+public class Libraryclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HybridCucumber/src/test/java/StepDefinition/Loginsteps.java
@@ -0,0 +1,10 @@
+package StepDefinition;
+
+public class Loginsteps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
